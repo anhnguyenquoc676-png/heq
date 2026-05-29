028f2a11108d0766284ab23ab4478cb32671bd89
anhnguyenquoc676-png <anhnguyenquoc676@gmail.com>
2026-05-29 08:27:08 +0700

    Create nghia.py

---
+++ nghia.py
@@ -0,0 +1,14 @@
+a = float(input("Nhập số thứ nhất: "))
+b = float(input("Nhập số thứ hai: "))
+
+# Tính toán
+tong = a + b
+hieu = a - b
+tich = a * b
+thuong = a / b
+
+# In kết quả
+print("Tổng:", tong)
+print("Hiệu:", hieu)
+print("Tích:", tich)
+print("Thương:", thuong)
